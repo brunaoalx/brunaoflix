ff799596868b83d6fe206e61008866ffd363dba7
brunaoalx <bruno.alexandre.ti@gmail.com>
2020-07-29 20:51:45 -0300

    Melhorando o carroseul, tirando a barra de scroll

--- brunaoflix/src/components/Carousel/components/Slider/index.js
+++ brunaoflix/src/components/Carousel/components/Slider/index.js
@@ -1,49 +1,65 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { VideoCardGroupContainer, Title, ExtraLink } from './styles';
-import VideoCard from './components/VideoCard';
-import Slider, { SliderItem } from './components/Slider';
+import SlickSlider from 'react-slick';
+import styled from 'styled-components';
 
-function Carousel({
-  ignoreFirstVideo,
-  category,
-}) {
-  const categoryTitle = category.titulo;
-  const categoryColor = category.cor;
-  const categoryExtraLink = category.link_extra;
-  const videos = category.videos;
-  return (
-    <VideoCardGroupContainer>
-      {categoryTitle && (
-        <>
-          <Title style={{ backgroundColor: categoryColor || 'red' }}>
-            {categoryTitle}
-          </Title>
-          {categoryExtraLink && 
-            <ExtraLink href={categoryExtraLink.url} target="_blank">
-              {categoryExtraLink.text}  
-            </ExtraLink>
-          }
-        </>
-      )}
-      <Slider>
-        {videos.map((video, index) => {
-          if (ignoreFirstVideo && index === 0) {
-            return null;
-          }
+const Container = styled.ul`
+  padding: 0;
+  margin: 0;
+  overflow-x:visible;
+  .slick-prev,
+  .slick-next {
+    z-index: 50;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    width: 30px;
+    height: 30px;
+    transform: initial;
+    &:before {
+      font-size: 30px;
+    }
+  }
+  
+  .slick-prev {
+    left: 0;
+  }
+  .slick-next {
+    right: 16px;
+  }
+`;
 
-          return (
-            <SliderItem key={video.titulo}>
-              <VideoCard
-                videoTitle={video.titulo}
-                videoURL={video.url}
-                categoryColor={categoryColor}
-              />
-            </SliderItem>
-          );
-        })}
-      </Slider>
-    </VideoCardGroupContainer>
-  );
-}
+export const SliderItem = styled.li`
+  margin-right: 5px;
+  a:hover {
+    transition:500ms;
+    transform:scale(1.2);
+    margin:0 30px 0 30px;
+  }
+  img {
+    margin: 16px;
+    width: 298px;
+    height: 197px;
+    object-fit: cover;
+  }
+`;
 
-export default Carousel;
+
+const Slider = ({ children }) => (
+  <Container>
+    <SlickSlider {...{
+      dots: false,
+      infinite: true,
+      speed: 300,
+      centerMode: false,
+      variableWidth: true,
+      adaptiveHeight: true,
+      swipeToSlide: true,
+    }}
+    >
+      {children}
+    </SlickSlider>
+  </Container>
+);
+
+export default Slider; 
